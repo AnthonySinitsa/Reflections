efa0ee6aa22d730ca1b8414ec7bb3cf1a0ea40e7
Anton <itonyemail@gmail.com>
2024-04-01 16:50:11 -0700

    showing albedo

--- Assets/Scripts/MyLightingShaderGUI.cs
+++ Assets/Scripts/MyLightingShaderGUI.cs
@@ -3,13 +3,24 @@ using UnityEditor;
 
 public class MyLightingShaderGUI : ShaderGUI{
 
+    MaterialEditor editor;
+    MaterialProperty[] properties;
+
     public override void OnGUI(
         MaterialEditor materialEditor, MaterialProperty[] properties
     ){
+        this.editor = editor;
+        this.properties = properties;
         DoMain();
     }
 
     void DoMain(){
         GUILayout.Label("Main Maps", EditorStyles.boldLabel);
+
+        MaterialProperty mainTex = FindProperty("_MainTex", properties);
+        GUIContent albedoLabel = new GUIContent(mainTex.displayName, "Albedo (RGB)");
+        MaterialProperty tint = FindProperty("_Tint", properties);
+        editor.TexturePropertySingleLine(albedoLabel, mainTex, tint);
+        editor.TextureScaleOffsetProperty(mainTex);
     }
 }
