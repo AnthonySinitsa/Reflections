584dbb43fcd5e58bc168ca027f0b327ed86d9845
Anton <itonyemail@gmail.com>
2024-04-01 15:19:25 -0700

    creating a lebel

--- Assets/Scripts/MyLightingShaderGUI.cs
+++ Assets/Scripts/MyLightingShaderGUI.cs
@@ -6,6 +6,10 @@ public class MyLightingShaderGUI : ShaderGUI{
     public override void OnGUI(
         MaterialEditor materialEditor, MaterialProperty[] properties
     ){
-        
+        DoMain();
+    }
+
+    void DoMain(){
+        GUILayout.Label("Main Maps", EditorStyles.boldLabel);
     }
 }
